b2bc3c302068c7fc7d8363073ecb67b496f79b95
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2025-03-24 14:01:28 +0900

    chore: 상태관리 라이브러리 설정 (#8)
    * docs: pr template 에 issue 번호 작성란 추가
    
    * chore: tanstack 쿼리 설치
    
    * chore: tanstack query lint 설정
    
    * feat: root layout 에 tanstack query provider 설정
    
    * feat: tanstack query provider test 추가
    
    * chore: zustand 설치
    
    게임 상태 설정은 추후에

---
+++ src/app/provider/__test__/tanstack-query.provider.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { getQueryClient } from "../tanstack-query.provider";
+import { describe, expect, it } from "vitest";
+import Providers from "../tanstack-query.provider";
+import { useQuery } from "@tanstack/react-query";
+
+const TestComponent = () => {
+  const { data } = useQuery({
+    queryKey: ['test'],
+    queryFn: () => Promise.resolve('data on client'),
+  });
+
+  return <div>{data}</div>;
+};
+
+describe('tanstack-query.provider', () => {
+  it('should render data from query that is fetched on server', async () => {
+    const queryClient = getQueryClient();
+    await queryClient.prefetchQuery({
+      queryKey: ['test'],
+      queryFn: () => Promise.resolve('data on server'),
+    });
+
+    render(
+      <Providers>
+        <TestComponent />
+      </Providers>,
+    );
+    expect(screen.getByText('data on server')).toBeDefined();
+  });
+});
