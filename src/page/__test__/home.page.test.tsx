ef1793646a20a6e94e5e20b3796e3ac243389417
ienrum <azang0506@gmail.com>
2025-03-24 13:02:23 +0900

    chore: vitest 설정 및 예시 추가

---
+++ src/page/__test__/home.page.test.tsx
@@ -0,0 +1,10 @@
+import HomePage from '../home.page';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+describe('HomePage', () => {
+  it('should render', () => {
+    render(<HomePage />);
+    expect(screen.getByText('HomePage')).toBeDefined();
+  });
+});
