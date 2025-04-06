945c4826f170018b2b19aaf4fef0371d54718ac5
ienrum <azang0506@gmail.com>
2025-04-06 13:54:19 +0900

    feat: leader board 컴포넌트 추가

---
+++ src/widgets/leader-board/ui/leader-board.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/src/shared/ui/card";
+import useGetRanking from "../api/use-get-ranking";
+import { useUser } from "@/src/shared/api/use-user";
+import { cn } from "@/lib/utils";
+import useInView from "@/src/shared/hook/use-intersection";
+import { useEffect } from "react";
+import formatDate from "@/src/shared/util/format-date";
+import { LEADER_BOARD_TITLE, LEADER_BOARD_DATE_TITLE, LEADER_BOARD_RANK_TITLE, LEADER_BOARD_NAME_TITLE, LEADER_BOARD_SCORE_TITLE } from "../config/constants";
+import formatTime from "@/src/shared/util/format-time";
+const LeaderBoard = () => {
+  const { data, fetchNextPage } = useGetRanking();
+  const { user } = useUser();
+  const { ref, isInView } = useInView();
+
+  const myRank = data.findIndex((item) => item.id === user?.id);
+  const today = new Date();
+  const formattedDate = formatDate(today);
+
+  useEffect(() => {
+    if (isInView) {
+      fetchNextPage();
+    }
+  }, [isInView, fetchNextPage]);
+
+  return (
+    <>
+      <Card className="w-full p-6 bg-white rounded-lg shadow-lg">
+        <CardHeader>
+          <h1 className="text-3xl font-bold text-center text-gray-800">{LEADER_BOARD_TITLE}</h1>
+        </CardHeader>
+        <CardTitle className="text-center text-gray-800">
+          <div className="text-gray-800">{formattedDate}</div>
+        </CardTitle>
+        <CardContent className="h-[60vh] overflow-y-scroll">
+          <div className="grid grid-cols-3 gap-4 p-4 rounded-lg font-semibold text-gray-700 w-full">
+            <div>{LEADER_BOARD_RANK_TITLE}</div>
+            <div>{LEADER_BOARD_NAME_TITLE}</div>
+            <div>{LEADER_BOARD_SCORE_TITLE}</div>
+          </div>
+          {data.map((item, index) => (
+            <div
+              key={item.id}
+              className={cn(
+                "grid grid-cols-3 gap-4 p-4 rounded-lg transition-all hover:bg-gray-50 w-full",
+                {
+                  "bg-amber-300": index === myRank,
+                }
+              )}
+            >
+              <div className="font-bold text-gray-700">{index + 1}</div>
+              <div className="text-gray-800">{item.name}</div>
+              <div className="text-gray-600">{formatTime(item.score)}</div>
+            </div>
+          ))}
+          <div ref={ref as React.RefObject<HTMLDivElement>} ></div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default LeaderBoard;
