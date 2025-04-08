34dfd4d1ba73f290a4444ca4775ec2976dcd7449
ienrum <azang0506@gmail.com>
2025-04-08 20:09:31 +0900

    feat: topbar 의 profile link 에 signin , siginout 기능 추가

---
+++ src/widgets/TopBar/ui/ProfileButton.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { createClient } from "@/utils/supabase/client"
+import { User } from "lucide-react"
+import { redirect } from "next/navigation"
+import Image from "next/image"
+import { useUser } from "@/src/shared/api/use-user"
+import { getQueryClient } from "@/src/app/utils/get-query-client"
+import { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip"
+
+const ProfileButton = () => {
+  const supabase = createClient()
+  const queryClient = getQueryClient()
+
+  const { user } = useUser()
+
+  const handleSignin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      }
+    })
+
+    if (error) {
+      console.error(error)
+    }
+
+    if (data.url) {
+      redirect(data.url)
+    }
+  }
+
+  const handleSignout = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      console.error(error)
+    }
+
+    queryClient.invalidateQueries({ queryKey: ['user'] })
+  }
+
+  if (!user) {
+    return (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="ghost" size="icon" className='hover:bg-gray-100' onClick={handleSignin}>
+              <User className="h-5 w-5" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Sign in</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    )
+  }
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="ghost" size="icon" className='hover:bg-gray-100' onClick={handleSignout}>
+            <Image src={user.user_metadata.avatar_url} alt="profile" width={32} height={32} className="rounded-full" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Sign out</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export default ProfileButton
