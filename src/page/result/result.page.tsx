4b0249ec375a58d48d72bce5ecd280b4c1263e40
ienrum <azang0506@gmail.com>
2025-04-11 13:49:12 +0900

    chore: deviationGraph 컴포넌트 임시 주석

--- src/page/result/result.page.tsx
+++ src/page/result/result.page.tsx
@@ -1,10 +1,10 @@
 import LeaderBoard from "@/src/widgets/leader-board/ui/leader-board";
-import DeviationGraph from "@/src/widgets/deviation-graph/ui/standard-deviation-graph";
+// import DeviationGraph from "@/src/widgets/deviation-graph/ui/standard-deviation-graph";
 
-const ResultPage = () => {
+const ResultPage = async () => {
   return (
     <div className="flex flex-col gap-4 items-center justify-center w-full h-full p-4">
-      <DeviationGraph />
+      {/* <DeviationGraph /> */}
       <LeaderBoard />
     </div>
   );
