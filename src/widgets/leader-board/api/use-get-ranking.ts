2a1e62fe362f5278492de42f5c3872cd16c0f941
ienrum <azang0506@gmail.com>
2025-04-11 13:49:39 +0900

    feat: game ranking fetch 훅 추가

--- src/widgets/leader-board/api/use-get-ranking.ts
+++ src/widgets/leader-board/api/use-get-ranking.ts
@@ -1,29 +1,32 @@
 import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
-import { RankingResponse, mockRankingResponse } from "../model/ranking-response";
+import { RankResponseDto } from "@/src/widgets/leader-board/model/dto/rank.dto";
+import { BaseResponseDto } from "@/src/app/model/backend/base-dto";
 
-const getRanking = async () => {
-  const response = await new Promise<RankingResponse>((resolve) => {
-    setTimeout(() => {
-      resolve(mockRankingResponse);
-    }, 1000);
-  });
-  return response;
+const PAGE_COUNT = 10;
+
+let myRank: number;
+
+const getRanking = async (gameId: string, pageParam: number) => {
+  const response = await fetch(`/api/game/result?gameId=${gameId}&from=${pageParam}&to=${pageParam + PAGE_COUNT}`);
+  const data = await response.json() as BaseResponseDto<RankResponseDto>;
+  myRank = data.data.myRank;
+  return data;
 };
 
-const useGetRanking = () => {
+const useGetRanking = (gameId: string) => {
   const { data, isLoading, error, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery({
-    queryKey: ["ranking"],
-    queryFn: () => getRanking(),
+    queryKey: ["ranking", gameId],
+    queryFn: ({ pageParam = 0 }) => getRanking(gameId, pageParam),
     getNextPageParam: (lastPage, pages) => {
-      return lastPage.data.length >= 10 ? pages.length + 1 : undefined;
+      return lastPage.data.rankList.length >= PAGE_COUNT ? pages.length + 1 : undefined;
     },
     initialPageParam: 0,
     select: (data) => {
-      return data.pages.flatMap((page) => page.data);
+      return data.pages.flatMap((page) => page.data.rankList);
     },
   });
 
-  return { data, isLoading, error, fetchNextPage, hasNextPage };
+  return { rankList: data, isLoading, error, fetchNextPage, hasNextPage, myRank };
 };
 
 export default useGetRanking;
