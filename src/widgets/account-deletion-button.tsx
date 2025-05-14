3dc1e81866cb716bb70285003dd71042b06dcd21
ienrum <azang0506@gmail.com>
2025-05-15 00:04:45 +0900

    feat: dialog 추가

--- src/widgets/account-deletion-button.tsx
+++ src/widgets/account-deletion-button.tsx
@@ -1,6 +1,15 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Dialog } from '@/components/ui/dialog';
 import { deleteAccount } from '@/src/entities/account/model';
 import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
@@ -12,20 +21,43 @@ const AccountDeletionButton = () => {
   const [isPending, startTransition] = useTransition();
 
   const handleDeleteAccount = async () => {
-    const { data } = await deleteAccount();
+    startTransition(async () => {
+      const { data } = await deleteAccount();
 
-    if (data) {
-      await queryClient.removeQueries();
-      router.push('/');
-    }
+      if (data) {
+        await queryClient.removeQueries();
+        router.push('/');
+      }
+    });
   };
 
   return (
-    <form action={handleDeleteAccount} className="flex w-full justify-end">
-      <Button className="pearl-hover bg-destructive/70" type="submit" disabled={isPending}>
-        {isPending ? 'Deleting...' : 'Delete Account'}
-      </Button>
-    </form>
+    <div className="flex justify-end">
+      <Dialog
+        title="Delete Account"
+        trigger={
+          <Button className="pearl-hover bg-destructive/70" disabled={isPending}>
+            Delete Account
+          </Button>
+        }
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Account</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your account and all of its data.
+          </DialogDescription>
+          <DialogFooter>
+            <form action={handleDeleteAccount}>
+              <Button className="pearl-hover bg-destructive/70" disabled={isPending}>
+                Delete Account
+              </Button>
+            </form>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
 
